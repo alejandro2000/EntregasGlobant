1c25e7a9bd4a9237a73ed95bb0cac24dfe01960f
Alejandro Taborda Cadavid <atabord@bancolombia.com.co>
2020-10-17 15:36:02 -0500

    Data population and test creation with its description.

--- finalTaeDelivery/src/test/java/com/globant/automation/tests/BookingTest.java
+++ finalTaeDelivery/src/test/java/com/globant/automation/tests/BookingTest.java
@@ -1,7 +1,33 @@
 package com.globant.automation.tests;
 
 import com.globant.automation.utils.tests.BaseTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 public class BookingTest extends BaseTest {
+
+    /**
+     *  Test consist in booking in a specific location
+     *  and adding all the information to do it, and the
+     *  required validations are.
+     *  -> Booking Hotel's title.
+     *  -> Booking review score.
+     *  -> Booking reservation price.
+     *  -> Booker name.
+     *  -> Booker email.
+     *  -> Book origin location.
+     */
+    @Test(description = "Booking to a destination id=00001", dataProvider = "dataProvider")
+    public void failedUserLogin(String location, String adults, String children, String childAge,
+                                String bedrooms,String bookerName, String bookerEmail, String originalLocation) {
+    }
+
+    @DataProvider
+    public Object[][] dataProvider() {
+        return new Object[][]{
+                new Object[]{"Bogotá, Colombia", "3", "1", "9", "1","Alejandro","alejandro.tabordacadavid@gmail.com",
+                        "Colombia"}
+        };
+    }
 
 }
